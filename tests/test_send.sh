e4d401b7de1e4e4ae3131e5f521eb4111787a26d
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2016-07-28 10:50:36 +0200

    renaming of project

--- tests/test_send.sh
+++ tests/test_send.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
- curl -i -v -H "$(cat headers)" --data "$(cat body)" http://127.0.0.1:8001
+#copy-paste contents for body/header files from GitHub hooks protocol
+curl -i -v -H "$(cat headers)" --data "$(cat body)" http://127.0.0.1:8001
